e2af2fa92ccd014e618012beccdd18b49f00a829
Olayiwola <olayiwolaajibode@gmail.com>
2024-11-27 13:07:27 -0600

    question display bug fixed

--- dashboard.php
+++ dashboard.php
@@ -18,7 +18,8 @@ include "header.php";
             <div class="row" style="margin-top: 30px">
 
                 <div class="row">
-                    <div class="col-lg-10 col-lg-push-1" style="min-height: 30px; background-color: white" id="load-questions">
+                    <div class="col-lg-10 col-lg-push-1" style="min-height: 30px; background-color: white"
+                        id="load-questions">
 
                     </div>
                 </div>
@@ -30,7 +31,7 @@ include "header.php";
                 <div class="col-lg-6 col-lg-push-3" style="min-height: 50px">
 
                     <div class="col-lg-12 text-center">
-                        <input type="button" class="btn btn-warning" value="Previous" onclick="load_previous();">$nbsp;
+                        <input type="button" class="btn btn-warning" value="Previous" onclick="load_previous();">&nbsp;
                         <input type="button" class="btn btn-success" value="Next" onclick="load_next();">
                     </div>
                 </div>
@@ -43,71 +44,63 @@ include "header.php";
 </div>
 
 <script type="text/javascript">
-    function load_total_que()
-    {
+    function load_total_que() {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                document.getElementById("total_que").innerHTML=xmlhttp.responseText;
+                document.getElementById("total_que").innerHTML = xmlhttp.responseText;
             }
         };
         xmlhttp.open("GET", "forajax/load_total_que.php", true);
         xmlhttp.send(null);
     }
 
-    var questionno="1";
+    var questionno = "1";
     load_questions(questionno);
 
-    function load_questions(questionno)
-    {
-        document.getElementById("current_que").innerHTML=questionno;
+    function load_questions(questionno) {
+        document.getElementById("current_que").innerHTML = questionno;
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
-            {
-                
-                if(xmlhttp.responseText=="over")
-                {
-                    window.location="result.php";
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+                if (xmlhttp.responseText == "over") {
+                    window.location = "result.php";
                 }
                 else {
-                    document.getElementById("load_questions").innerHTML=xmlhttp.responseText;
+                    document.getElementById("load-questions").innerHTML = xmlhttp.responseText;
                     load_total_que(null);
                 }
             }
         };
-        xmlhttp.open("GET", "forajax/load_questions.php?questionno="+questionno, true);
-        xmlhttp.send(null); 
+        xmlhttp.open("GET", "forajax/load_questions.php?questionno=" + questionno, true);
+        xmlhttp.send(null);
     }
 
-    function radioclick(radiovalue, questionno)
-    {
+    function radioclick(radiovalue, questionno) {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                
+
             }
         };
-        xmlhttp.open("GET", "forajax/save_answer_in_session.php?questionno="+questionno +"&value1="+radiovalue, true);
+        xmlhttp.open("GET", "forajax/save_answer_in_session.php?questionno=" + questionno + "&value1=" + radiovalue, true);
         xmlhttp.send(null);
-        
+
     }
 
-    function load_previous()
-    {
-        if(questionno=="1")
-        {
+    function load_previous() {
+        if (questionno == "1") {
             load_questions(questionno);
         }
-        else{
-            questionno=eval(questionno)-1;
+        else {
+            questionno = eval(questionno) - 1;
             load_questions(questionno);
         }
     }
 
-    function load_next()
-    {
-        questionno=eval(questionno)+1;
+    function load_next() {
+        questionno = eval(questionno) + 1;
         load_questions(questionno);
     }
 </script>
